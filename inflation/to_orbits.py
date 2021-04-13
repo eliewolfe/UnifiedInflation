e4651df85c405820fb8baf6d6e882374bdcee817
Bora-Ulu <bora.ulu@ug.bilkent.edu.tr>
2021-04-13 16:47:31 +0300

    Working on the AMatrix and the inflation matrix.

--- inflation/to_orbits.py
+++ inflation/to_orbits.py
@@ -20,8 +20,8 @@ else:
     cached_property = property
     
 from wrapper import *
-from internal_functions.groups import dimino_sympy, orbits_of_object_under_group_action
-from internal_functions.utilities import MoveToFront, MoveToBack
+from internal_functions.groups import dimino_sympy, orbits_of_object_under_group_action, minimize_object_under_group_action
+from internal_functions.utilities import MoveToFront, MoveToBack,SparseMatrixFromRowsPerColumn
 
 
 class inflated_hypergraph:
@@ -92,7 +92,9 @@ class packed_inflated_columns(inflated_hypergraph,DAG):
     
     @cached_property
     def column_orbits(self):
-        return orbits_of_object_under_group_action(self.shaped_packed_column_integers,self.inflation_group_elements).T
+        AMatrix=orbits_of_object_under_group_action(self.shaped_packed_column_integers,self.inflation_group_elements).T
+        AMatrix = np.compress(AMatrix[0]>=0, AMatrix, axis=1)
+        return AMatrix
 
 class expressible_sets(packed_inflated_columns):
     
@@ -104,15 +106,10 @@ class expressible_sets(packed_inflated_columns):
         #print(self.conf_setting_integers)
         self.conf_setting_indecies=self.conf_setting_integers[np.repeat(np.arange(len(self.conf_setting_integers)),self.inflation_copies)]
         self.ravelled_conf_setting_indecies=[i for j in self.conf_setting_indecies for i in j]
-        print(self.ravelled_conf_setting_indecies)
+        #print(self.ravelled_conf_setting_indecies)
         
         self.ravelled_conf_var_indecies=np.repeat(np.arange(self.observed_count),np.array(self.setting_cardinalities)*np.array(self.inflation_copies))
-
-        """
-        self.unpacked_ninflated_conf_integers=np.array([list(np.arange(self.setting_cardinalities[obs])+np.array(self.setting_cardinalities)[np.arange(self.observed_count)[:obs_ind]].sum()) for obs_ind,obs in enumerate(range(self.observed_count))],dtype=object)
-        self.unpacked_conf_integers_indecies=self.unpacked_ninflated_conf_integers[np.repeat(np.arange(len(self.unpacked_ninflated_conf_integers)),self.inflation_copies)]
-        self.ravelled_unpacked_conf_integers_indecies=[i for j in self.unpacked_conf_integers_indecies for i in j]
-        """
+        #print(self.ravelled_conf_var_indecies)
         
         self.unpacked_inflated_copies=[self.setting_cardinalities[observable]*self.inflation_copies[observable] for observable in range(self.observed_count)]
         self.inflated_unpacked_cardinalities=list(itertools.chain.from_iterable([list(np.repeat(outcome_cardinalities[observable],self.unpacked_inflated_copies[observable])) for observable in range(self.observed_count)]))
@@ -128,57 +125,111 @@ class expressible_sets(packed_inflated_columns):
         self.partitioned_unpacked_eset_candidates=[list(itertools.product(*list(self.unpacked_conf_integers[part]))) for part in self.packed_partitioned_eset]
         #print(self.partitioned_unpacked_eset_candidates)
         self.partitioned_unpacked_esets=list(itertools.product(*self.partitioned_unpacked_eset_candidates))
-        #print(self.partitioned_unpacked_esets)
+        #print(len(self.partitioned_unpacked_esets))
+        self.flat_unpacked_esets=[[elem for part in eset for elem in part] for eset in self.partitioned_unpacked_esets]
+        #print(self.flat_unpacked_esets)
         
-        def _valid_outcomes(self,eset_part_candidate):
-            observables=np.array(self.ravelled_conf_var_indecies)[np.array(eset_part_candidate)]
-            settings_assignment=tuple(np.array(self.ravelled_conf_setting_indecies)[np.array(eset_part_candidate)])
-            outcome_assignments=np.ndindex(self.outcomes_cardinalities)
-            for assignment in outcome_assignments:
-                validity=True
-                for i in range(len(settings_assignment)):
-                    setting_integer=settings_assignment[i]
-                    setting_shape=self.shaped_setting_cardinalities[observables[i]]
-                    parents_of=self.inverse_directed_structure[observables[i]]
-                    settings_of_v = np.unravel_index(setting_integer, setting_shape)
-                    outcomes_relevant_to_v = np.compress(parents_of, assignment)
-                    validity=validity and np.array_equal(settings_of_v[1:], outcomes_relevant_to_v)
-                yield validity
-        def valid_outcomes(self,eset_part_candidate):
-            return np.fromiter(_valid_outcomes(self,eset_part_candidate), np.bool)
+    def _valid_outcomes(self,eset_part_candidate):
+        observables=np.array(self.ravelled_conf_var_indecies)[np.array(eset_part_candidate)]
+        settings_assignment=tuple(np.array(self.ravelled_conf_setting_indecies)[np.array(eset_part_candidate)])
+        outcome_assignments=np.ndindex(self.outcomes_cardinalities)
+        for assignment in outcome_assignments:
+            validity=True
+            for i in range(len(settings_assignment)):
+                setting_integer=settings_assignment[i]
+                setting_shape=self.shaped_setting_cardinalities[observables[i]]
+                parents_of=self.inverse_directed_structure[observables[i]]
+                settings_of_v = np.unravel_index(setting_integer, setting_shape)
+                outcomes_relevant_to_v = np.compress(parents_of, assignment)
+                validity=validity and np.array_equal(settings_of_v[1:], outcomes_relevant_to_v)
+            yield validity
+    def valid_outcomes(self,eset_part_candidate):
+        return np.fromiter(self._valid_outcomes(eset_part_candidate), np.bool)
         
-        def eset_row_monomials(self,eset):
-            validoutcomes=[valid_outcomes(self,part) for part in eset]
-            
-            v=validoutcomes[-1]
-            for i in range(len(validoutcomes)-1):
-                v=np.kron(validoutcomes[len(validoutcomes)-1-i],v)
-            
-            monomials=((np.arange(len(v))+1)*v.astype(np.int))-1
+    def eset_unpacking_rows_to_keep(self,partitioned_eset):
+        validoutcomes=[self.valid_outcomes(part) for part in partitioned_eset]
+        
+        v=validoutcomes[-1]
+        for i in range(len(validoutcomes)-1):
+            v=np.kron(validoutcomes[len(validoutcomes)-1-i],v)
+        
+        eset_kept_rows=((np.arange(len(v))+1)*v.astype(np.int))-1
+        eset_kept_rows=eset_kept_rows[np.where(eset_kept_rows!=-1)[0]]
+        
+        return eset_kept_rows
+        
+        
+    def eset_symmetry_group(self,partitioned_eset):
+         
+        when_sorted = np.argsort(np.hstack(partitioned_eset))
+        it = iter(when_sorted)
+        _canonical_pos2 = [list(itertools.islice(it, size)) for size in self.inflation_minima]
+
+        unfiltered_variants = np.array([np.hstack(np.vstack(perm).T) for perm in
+                                        itertools.permutations(itertools.zip_longest(*_canonical_pos2, fillvalue=-1))])
+        expressible_set_variants_filter = np.add(unfiltered_variants, 1).astype(np.bool)
+        unfiltered_variants = unfiltered_variants.compress(
+            (expressible_set_variants_filter == np.atleast_2d(expressible_set_variants_filter[0])).all(axis=1),
+            axis=0)
+        filtered_variants = np.array([eset.compress(np.add(eset, 1).astype(np.bool)) for eset in unfiltered_variants])
+        return np.take_along_axis(np.argsort(filtered_variants,axis=1), np.atleast_2d(when_sorted),   axis=1)
     
-            return monomials
-            
-            
-            
-        print(eset_row_monomials(self,self.partitioned_unpacked_esets[0]))
-        #print(valid_outcomes(self,self.partitioned_unpacked_eset_candidates[0][0]))
-        
-
-        
-    def Columns_to_unique_rows(self, shaped_column_integers,flat_eset):
-                data_shape = shaped_column_integers.shape
-                # Can be used for off-diagonal expressible sets with no adjustment!
-                expr_set_size = np.take(data_shape, flat_eset).prod()
+    def eset_symmetry_rows_to_keep(self,partitioned_eset,shape_of_eset,size_of_eset):
+        which_rows_to_keep = np.arange(size_of_eset).reshape(shape_of_eset)
+        minimize_object_under_group_action(
+            which_rows_to_keep,
+            self.eset_symmetry_group(partitioned_eset), skip=1)
+        which_rows_to_keep = np.unique(which_rows_to_keep.ravel(), return_index=True)[1]
+        return which_rows_to_keep
     
-                reshaped_column_integers = shaped_column_integers.transpose(
-                    MoveToBack(len(data_shape), flat_eset)).reshape(
-                    (-1, expr_set_size))
-                encoding_of_columns_to_monomials = np.empty(shaped_column_integers.size, np.int)
-                encoding_of_columns_to_monomials[reshaped_column_integers] = np.arange(expr_set_size)
-                return encoding_of_columns_to_monomials
-
-
-
+    
+    def eset_disgarded_rows_to_trash(self,partitioned_eset,offset):
+        shape_of_eset = np.take(np.array(self.inflated_unpacked_cardinalities), [elem for part in partitioned_eset for elem in part])
+        size_of_eset = shape_of_eset.prod()
+        which_rows_to_keep=np.intersect1d(self.eset_unpacking_rows_to_keep(partitioned_eset),self.eset_symmetry_rows_to_keep(partitioned_eset,shape_of_eset,size_of_eset))
+        size_of_eset_after_symmetry_and_unpacking = len(which_rows_to_keep)
+        #there_are_discarded_rows = (size_of_eset_after_symmetry_and_unpacking < size_of_eset)
+        discarded_rows_to_the_back = np.full(size_of_eset, -(offset+1), dtype=np.int)
+        np.put(discarded_rows_to_the_back, which_rows_to_keep, np.arange(size_of_eset_after_symmetry_and_unpacking))
+        discarded_rows_to_the_back=discarded_rows_to_the_back+offset
+        return discarded_rows_to_the_back
+    
+    
+    def columns_to_unique_rows(self, shaped_column_integers,flat_eset):
+        data_shape = shaped_column_integers.shape
+        # Can be used for off-diagonal expressible sets with no adjustment!
+        flat_eset=np.array(flat_eset)
+        expr_set_size = np.take(data_shape, flat_eset).prod()
+        reshaped_column_integers = shaped_column_integers.transpose(MoveToBack(len(data_shape), flat_eset)).reshape((-1, expr_set_size))            
+        encoding_of_columns_to_monomials = np.empty(shaped_column_integers.size, np.int)
+        encoding_of_columns_to_monomials[reshaped_column_integers] = np.arange(expr_set_size)
+        return encoding_of_columns_to_monomials
+    
+    def AMatrix(self):
+        offset=0
+        amatrices=[]
+        for partitioned_eset in self.partitioned_unpacked_esets:
+            disgarded=self.eset_disgarded_rows_to_trash(partitioned_eset,offset)
+            amatrix=disgarded.take(self.columns_to_unique_rows(self.shaped_unpacked_column_integers,[elem for part in partitioned_eset for elem in part])).take(self.column_orbits)
+            offset=np.amax(disgarded)+1
+            amatrices.append(amatrix)
+        
+        AMatrix=np.vstack(tuple(amatrices))
+        trash=np.amax(AMatrix)+1
+        trash_positions=np.where(AMatrix==-1)
+        AMatrix[trash_positions[0],trash_positions[1]]=trash
+        
+        return AMatrix
+    
+class inflation_problem(expressible_sets):
+    
+    def __init__(self,hypergraph,inflation_orders,directed_structure, outcome_cardinalities, private_setting_cardinalities):
+        expressible_sets.__init__(self,hypergraph,inflation_orders,directed_structure, outcome_cardinalities, private_setting_cardinalities)
+        
+    def inflation_matrix(self):
+        InfMat=SparseMatrixFromRowsPerColumn(self.AMatrix()).asformat('csr', copy=False)
+        return InfMat[:-1]
+        
 if __name__ == '__main__':
     
     hypergraph=np.array([[1,1,0],[0,1,1]])
@@ -187,6 +238,9 @@ if __name__ == '__main__':
     private_setting_cardinalities = (2, 1, 1)
     
     inflation_orders=[2,2]
-    print(expressible_sets(hypergraph,inflation_orders,directed_structure, outcome_cardinalities, private_setting_cardinalities))
-    
+    p=((0, 4, 12), (2, 10, 14))
+    e=expressible_sets(hypergraph,inflation_orders,directed_structure, outcome_cardinalities, private_setting_cardinalities)
+    inf=inflation_problem(hypergraph,inflation_orders,directed_structure, outcome_cardinalities, private_setting_cardinalities)
+    #print(e.AMatrix())
+    print(inf.inflation_matrix().shape)
     
