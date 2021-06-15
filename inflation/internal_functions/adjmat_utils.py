2aa82e9a76fe84cb780b0ed136e77df3acd26ed3
Elie Wolfe <elupus@gmail.com>
2021-06-14 20:16:42 -0400

    transitive reduction from transitive closure added

--- inflation/internal_functions/adjmat_utils.py
+++ inflation/internal_functions/adjmat_utils.py
@@ -4,7 +4,7 @@ import numpy as np
 
 def transitive_closure(adjmat):
     n=len(adjmat)
-    closure_mat = np.asarray(adjmat, dtype=bool)+np.identity(n, dtype=bool)
+    closure_mat = np.bitwise_or(np.asarray(adjmat, dtype=bool),np.identity(n, dtype=bool))
     while n>0:
         n = np.floor_divide(n,2)
         next_closure_mat = np.matmul(closure_mat, closure_mat)
@@ -12,14 +12,17 @@ def transitive_closure(adjmat):
             break
         else:
             closure_mat=next_closure_mat
-    return closure_mat
+    return np.bitwise_and(closure_mat,np.invert(np.identity(len(adjmat), dtype=bool)))
 
 def transitive_reduction(adjmat):
-    return True
-    #TODO: One can compute the reduction from the closure, per  https://en.wikipedia.org/wiki/Transitive_reduction#Computing_the_reduction_using_the_closure
+    n = len(adjmat)
+    closure_mat=transitive_closure(adjmat)
+    #closure_minus_identity = np.bitwise_and(transitive_closure(adjmat),np.invert(np.identity(n, dtype=bool)))
+    return np.bitwise_and(closure_mat,np.invert(np.matmul(closure_mat, closure_mat)))
 
 if __name__ == '__main__':
-    adjmat = np.asarray(np.array([[1,0,	1,	1,	0,	0],
+    adjmat = np.asarray(np.array(
+    [[1,0,	1,	1,	0,	0],
     [0,	1,	0,	0,	1,	1],
     [0,	1,	1,	0,	0,	0],
     [0,	0,	0,	1,	0,	0],
@@ -27,4 +30,5 @@ if __name__ == '__main__':
     [0,	0,	0,	0,	0,	1]],dtype=int),dtype=bool)+np.identity(6, dtype=bool)
 
     print(transitive_closure(adjmat).astype(int))
+    print(transitive_reduction(adjmat).astype(int))
     # print(np.linalg.matrix_power(adjmat, 2).astype(np.int_))
